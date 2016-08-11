7654cbe9a075ad3d943c3220ea72048db8f8d81d
asif_agiliztech <asif@agiliztech.com>
2016-08-11 17:23:58 +0530

    another commiting (Added Activity.java, layout, and images)

---
+++ app/src/main/java/com/agiliztech/musicescape/activity/MoodMappingActivity.java
@@ -0,0 +1,15 @@
+package com.agiliztech.musicescape.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.agiliztech.musicescape.R;
+
+public class MoodMappingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_mapping);
+    }
+}
