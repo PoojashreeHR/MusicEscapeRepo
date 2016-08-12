e940c016d03e79d6c4570a1ce9ca986ba7abb589
Mayan_Gorky <amritha.mayan@agiliztech.com>
2016-08-12 15:56:49 +0530

    Model Class

---
+++ app/src/main/java/com/agiliztech/musicescape/Models/Journey.java
@@ -0,0 +1,45 @@
+package com.agiliztech.musicescape.Models;
+
+/**
+ * Created by amrithamayangorky on 8/12/16.
+ */
+public class Journey {
+    private String generatedBy;
+   // NSData * dots;
+    // NSData * points;
+    private int trackCount;
+    private String name;
+    private JourneySession sessions;
+
+    public String getGeneratedBy() {
+        return generatedBy;
+    }
+
+    public void setGeneratedBy(String generatedBy) {
+        this.generatedBy = generatedBy;
+    }
+
+    public int getTrackCount() {
+        return trackCount;
+    }
+
+    public void setTrackCount(int trackCount) {
+        this.trackCount = trackCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public JourneySession getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(JourneySession sessions) {
+        this.sessions = sessions;
+    }
+}
---
+++ app/src/main/java/com/agiliztech/musicescape/Models/JourneySongs.java
@@ -0,0 +1,62 @@
+package com.agiliztech.musicescape.Models;
+
+/**
+ * Created by amrithamayangorky on 8/12/16.
+ */
+public class JourneySongs {
+
+    private int playedCount;
+    private int  skipped;
+    private int  swapped;
+    private int  trackNo;
+    private JourneySession session;
+    private Song song;
+
+    public int getPlayedCount() {
+        return playedCount;
+    }
+
+    public void setPlayedCount(int playedCount) {
+        this.playedCount = playedCount;
+    }
+
+    public int getSkipped() {
+        return skipped;
+    }
+
+    public void setSkipped(int skipped) {
+        this.skipped = skipped;
+    }
+
+    public int getSwapped() {
+        return swapped;
+    }
+
+    public void setSwapped(int swapped) {
+        this.swapped = swapped;
+    }
+
+    public int getTrackNo() {
+        return trackNo;
+    }
+
+    public void setTrackNo(int trackNo) {
+        this.trackNo = trackNo;
+    }
+
+    public JourneySession getSession() {
+        return session;
+    }
+
+    public void setSession(JourneySession session) {
+        this.session = session;
+    }
+
+    public Song getSong() {
+        return song;
+    }
+
+    public void setSong(Song song) {
+        this.song = song;
+    }
+}
