0cdd99063f207fc485c451e66165109df9458e39
Mayan_Gorky <amritha.mayan@agiliztech.com>
2016-08-24 20:22:19 +0530

    Setting_Page_Activity in Dashboard

--- app/src/main/java/com/agiliztech/musicescape/activity/DashboardActivity.java
+++ app/src/main/java/com/agiliztech/musicescape/activity/DashboardActivity.java
@@ -5,21 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.agiliztech.musicescape.R;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    ImageView menu_activeSettings,menu_settings;
 
     private ImageView settingsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
+        menu_activeSettings = (ImageView) findViewById(R.id.menu_activeSettings);
         settingsButton = (ImageView) findViewById(R.id.menu_settings);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                menu_activeSettings.setVisibility(View.VISIBLE);
                 Intent intent = new Intent(DashboardActivity.this,SettingsActivity.class);
                 startActivity(intent);
                 finish();
