d318a391879f2d9f6748385be0b2eb5ff70cd131
Mayan_Gorky <amritha.mayan@agiliztech.com>
2016-09-04 20:40:59 +0530

    Library Fix

--- app/src/main/java/com/agiliztech/musicescape/activity/LibraryActivity.java
+++ app/src/main/java/com/agiliztech/musicescape/activity/LibraryActivity.java
@@ -258,6 +258,8 @@ public class LibraryActivity extends BaseMusicActivity implements View.OnClickLi
                 //songScan.setBackgroundResource(R.drawable.menu_buttons_song_scan_active);
                 songScan.setFocusableInTouchMode(false);
                 songScan.setFocusable(false);
+                Intent intent = new Intent(LibraryActivity.this, MoodMappingActivity.class);
+                startActivity(intent);
                 finish();
             }
         });
