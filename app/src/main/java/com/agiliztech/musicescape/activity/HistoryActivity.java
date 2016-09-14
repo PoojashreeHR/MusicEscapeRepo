2d9244734c0a109e94e7f727450a80c5b15a3fa0
Mayan_Gorky <amritha.mayan@agiliztech.com>
2016-09-14 23:35:19 +0530

    Save Playlist From History

--- app/src/main/java/com/agiliztech/musicescape/activity/HistoryActivity.java
+++ app/src/main/java/com/agiliztech/musicescape/activity/HistoryActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -45,14 +46,14 @@ public class HistoryActivity extends BaseMusicActivity {
         dashboardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HistoryActivity.this,NewDashboardActivity.class);
+                Intent intent = new Intent(HistoryActivity.this, NewDashboardActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.history_rv);
-        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         recyclerView.setAdapter(new HistoryAdapter(getHistoryData()));
     }
 
@@ -63,8 +64,8 @@ public class HistoryActivity extends BaseMusicActivity {
     }
 
     @Override
-    public void onResume()
-    {    super.onResume();
+    public void onResume() {
+        super.onResume();
         //   settings.edit().putBoolean("is_first_time", false).commit();
         getSharedPreferences("DashboardPreference", MODE_PRIVATE).edit()
                 .putBoolean("history", false).commit();
@@ -79,16 +80,16 @@ public class HistoryActivity extends BaseMusicActivity {
         return px;
     }
 
-    public int adjustWidth(int dp560){
+    public int adjustWidth(int dp560) {
         DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
         int dp = dp560 * displayMetrics.widthPixels / 560;
-        return (int) UtilityClass.convertPixelsToDpWidth(dp,this);
+        return (int) UtilityClass.convertPixelsToDpWidth(dp, this);
         //return  dp;
     }
     private class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>
 
     {
-        private  List<JourneySession> sessions;
+        private List<JourneySession> sessions;
 
         public HistoryAdapter(List<JourneySession> journeySessionList) {
             this.sessions = journeySessionList;
@@ -108,8 +109,7 @@ public class HistoryActivity extends BaseMusicActivity {
             if (pos % 2 == 0) {
                 holder.right_view.setVisibility(View.VISIBLE);
                 // pos++;
-            }
-            else{
+            } else {
                 holder.right_view.setVisibility(View.GONE);
                 // pos--;
             }
@@ -119,7 +119,7 @@ public class HistoryActivity extends BaseMusicActivity {
             holder.journeyView.setJourneyPoints(session.getJourney().getJourneyDotsArray());
             holder.journeyView.setEnabled(false);
 
-            holder.journeyView.setPadding(dpToPx(18),0, dpToPx(18),0);
+            holder.journeyView.setPadding(dpToPx(18), 0, dpToPx(18), 0);
 
             holder.tv_currentmood.setTextColor(SongsManager.colorForMood(session.getCurrentMood()));
             holder.tv_targetmood.setTextColor(SongsManager.colorForMood(session.getTargetMood()));
@@ -128,7 +128,7 @@ public class HistoryActivity extends BaseMusicActivity {
             holder.tv_targetmood.setText(SongsManager.textForMood(SongsManager.getIntValue(session.getTargetMood())));
 
 
-            Date myDate= session.getStarted();
+            Date myDate = session.getStarted();
             String formatedDate = "";
             try {
                 SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM");
@@ -142,31 +142,34 @@ public class HistoryActivity extends BaseMusicActivity {
                 @Override
                 public void onClick(View v) {
 
-                    final Dialog dialogs = new Dialog(context);
-                    LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    View layout = inflater.inflate(R.layout.dialog_save_history, null);
-                    dialogs.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                    dialogs.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                    Button btn_save = (Button) layout.findViewById(R.id.btn_save);
-                    btn_save.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            saveSession(session);
-                            dialogs.dismiss();
-                        }
-                    });
-                    Button btn_cancel = (Button) layout.findViewById(R.id.btn_cancel);
-                    btn_cancel.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            dialogs.dismiss();
-                        }
-                    });
+                    if (isJourneyAlreadySaved(session)) {
+                    } else {
+                        final Dialog dialogs = new Dialog(context);
+                        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                        View layout = inflater.inflate(R.layout.dialog_save_history, null);
+                        dialogs.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                        dialogs.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                        Button btn_save = (Button) layout.findViewById(R.id.btn_save);
+                        btn_save.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                saveSession(session);
+                                dialogs.dismiss();
+                            }
+                        });
+                        Button btn_cancel = (Button) layout.findViewById(R.id.btn_cancel);
+                        btn_cancel.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialogs.dismiss();
+                            }
+                        });
 
-                    dialogs.setContentView(layout);
-                    dialogs.show();
+                        dialogs.setContentView(layout);
+                        dialogs.show();
 
                 /*dialogs.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,800);*/
+                    }
                 }
             });
 
@@ -178,36 +181,34 @@ public class HistoryActivity extends BaseMusicActivity {
         }
 
 
+        class HistoryViewHolder extends RecyclerView.ViewHolder {
+
+            FrameLayout overlay;
+            JourneyView journeyView;
+            TextView tv_currentmood, tv_targetmood, tv_playlistCreated;
+            View right_view;
+            LinearLayout ll_saveHistory;
+
+            public HistoryViewHolder(View itemView) {
+                super(itemView);
+                journeyView = (JourneyView) itemView.findViewById(R.id.journey);
+                overlay = (FrameLayout) itemView.findViewById(R.id.overlay);
+                tv_currentmood = (TextView) itemView.findViewById(R.id.tv_currentmood);
+                tv_targetmood = (TextView) itemView.findViewById(R.id.tv_targetmood);
+                tv_playlistCreated = (TextView) itemView.findViewById(R.id.tv_playlistCreated);
+                right_view = (View) itemView.findViewById(R.id.right_view);
+                ll_saveHistory = (LinearLayout) itemView.findViewById(R.id.ll_saveHistory);
 
 
+            }
+        }
+    }
 
-         class HistoryViewHolder extends  RecyclerView.ViewHolder {
-
-             FrameLayout overlay;
-             JourneyView journeyView;
-              TextView tv_currentmood,tv_targetmood,tv_playlistCreated;
-             View right_view;
-             LinearLayout ll_saveHistory;
-
-             public HistoryViewHolder(View itemView) {
-                 super(itemView);
-                 journeyView = (JourneyView) itemView.findViewById(R.id.journey);
-                 overlay = (FrameLayout) itemView.findViewById(R.id.overlay);
-                 tv_currentmood = (TextView) itemView.findViewById(R.id.tv_currentmood);
-                 tv_targetmood = (TextView) itemView.findViewById(R.id.tv_targetmood);
-                 tv_playlistCreated = (TextView) itemView.findViewById(R.id.tv_playlistCreated);
-                 right_view = (View) itemView.findViewById(R.id.right_view);
-                 ll_saveHistory = (LinearLayout) itemView.findViewById(R.id.ll_saveHistory);
-
-
-
-
-
-
-
-
-             }
-         }
+    private boolean isJourneyAlreadySaved(JourneySession session) {
+        JourneySessionDBHelper journeySessionDBHelper = new JourneySessionDBHelper(this);
+        boolean isExists = journeySessionDBHelper.isFavJourneySession(session);
+        journeySessionDBHelper.close();
+        return isExists;
     }
 
     private void saveSession(JourneySession session) {
@@ -219,7 +220,7 @@ public class HistoryActivity extends BaseMusicActivity {
     private void startPlaylistviewWithJourney(JourneySession session) {
         JourneyService journeyService = JourneyService.getInstance(this);
         journeyService.setCurrentSession(session);
-        if(musicSrv != null) {
+        if (musicSrv != null) {
             if (musicSrv.isPng()) {
                 musicSrv.pausePlayer();
             }
@@ -235,7 +236,7 @@ public class HistoryActivity extends BaseMusicActivity {
 
     private Size getGapsSize() {
         DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-        return new Size(0.92500000000000004f*displayMetrics.widthPixels/560f, 0.96999999999999997f*displayMetrics.heightPixels/560f);
+        return new Size(0.92500000000000004f * displayMetrics.widthPixels / 560f, 0.96999999999999997f * displayMetrics.heightPixels / 560f);
 
     }
 }
